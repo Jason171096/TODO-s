28a2da8784e4e0c9624cc4c883a1d5c2ce6f2ee7
Jason171096 <bboyace10@gmail.com>
2021-10-06 20:38:55 -0500

    Added gif

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -1,23 +1,36 @@
-import React, { useContext } from 'react';
-import './TodoCounter.css'
-import TaskIco from '../../img/task-list.svg'
- 
-const TodoCounter = ({ completedTodos, totalTodos }) => {
-    return (
-        <div className="TodoCounter">
-            <div className="title">
-                <h1>¡Welcome User!</h1>
-                <div className="sub-title">
-                    <div>
-                        <img width='40px' height='40px' src={TaskIco} alt="Image tasks list"/>
-                        <p>Tasks List</p>
-                    </div>
-                    <h2>Today</h2>
-                    <p>Completed: {completedTodos}/{totalTodos}</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+import React, { useContext } from "react";
+import "./TodoCounter.css";
+import TaskIco from "../../img/task-list.svg";
+import taskListAnimated from "../../img/task-list.json";
+import Lottie from "react-lottie";
 
-export { TodoCounter }
+const TodoCounter = ({ completedTodos, totalTodos }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: taskListAnimated,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div className="TodoCounter">
+      <div className="title">
+          <div>
+          <h1>¡Welcome, Jason!</h1>
+          <p>Organize your tasks</p>
+          </div>
+        <Lottie options={defaultOptions} height={150} width={250} />
+      </div>
+      <div className="sub-title">
+        <p>Tasks List</p>
+        <h2>Today</h2>
+        <p>
+          Completed: {completedTodos}/{totalTodos}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export { TodoCounter };
